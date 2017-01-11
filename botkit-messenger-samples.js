c2b48986ddc01a218cb321f9e0285c9e52e40cfd
Josh <josh@smalltalk.ai>
2017-01-11 15:41:19 -0800

    add support for `help`

--- botkit-messenger-samples.js
+++ botkit-messenger-samples.js
@@ -1,1 +1,57 @@
 'use strict';
+
+var MessengerSamples = function(controller, bot, config) {
+  config = config || {};
+  var
+    prefix = config.prefix || 'sample',
+    hearPattern = '^' + prefix + ':',
+    regex = new RegExp(hearPattern, 'i'),
+    requests = [
+      'account linking',
+      'audio',
+      'button',
+      'file',
+      'generic',
+      'gif',
+      'image',
+      'image:large',
+      'image:tall',
+      'list',
+      'list:compact',
+      'quick reply',
+      'read receipt',
+      'receipt',
+      'typing on',
+      'typing off',
+      'video'
+    ]
+  ;
+
+  controller.hears(hearPattern, 'message_received', function(bot, message) {
+    var
+      request = message.text.replace(regex, '')
+    ;
+    console.log(message);
+    switch (request) {
+      case 'help':
+        var reply = requests.map(function(item) {
+          return prefix + ':' + item;
+        }).join('\n');
+        return bot.reply(message, reply);
+      case 'image':
+
+        break;
+      case 'image:large':
+
+        break;
+      case 'image:tall':
+
+        break;
+      default:
+
+    }
+    bot.reply(message, request);
+  });
+};
+
+module.exports = MessengerSamples;
